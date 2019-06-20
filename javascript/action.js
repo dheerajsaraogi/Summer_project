09f62d6ac36725605739286dbc123de79eca1938
dheerajsaraogi <37182085+dheerajsaraogi@users.noreply.github.com>
2019-06-20 09:26:55 +0530

    Added login and animation

--- javascript/action.js
+++ javascript/action.js
@@ -5,4 +5,15 @@ $(window).scroll(function() {
     } else {
         $('.fixed-top').css('background', 'rgba(0, 0, 0, 0.7)');
     }
+    if (scroll > 820) {
+
+        document.querySelector('.card1').classList.add('animated', 'fadeInLeft');
+        document.querySelector('.card2').classList.add('animated', 'fadeInDown');
+        document.querySelector('.card3').classList.add('animated', 'fadeInRight');
+    }
+    if (scroll > 1200) {
+        document.querySelector('.card4').classList.add('animated', 'fadeInLeft');
+        document.querySelector('.card5').classList.add('animated', 'fadeInUp');
+        document.querySelector('.card6').classList.add('animated', 'fadeInRight');
+    }
 });
