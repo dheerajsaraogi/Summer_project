ad8c4840adbcd9151b4dda336914c2eadf4e82c0
dheerajsaraogi <37182085+dheerajsaraogi@users.noreply.github.com>
2019-06-21 14:17:40 +0530

    Add files via upload

--- javascript/action.js
+++ javascript/action.js
@@ -5,15 +5,22 @@ $(window).scroll(function() {
     } else {
         $('.fixed-top').css('background', 'rgba(0, 0, 0, 0.7)');
     }
+    if (scroll > 50) {
+        document.querySelectorAll('.animate_about')[0].classList.add('animated', 'slideInUp');
+        document.querySelectorAll('.animate_about')[1].classList.add('animated', 'slideInUp');
+
+
+    }
+
     if (scroll > 820) {
 
         document.querySelector('.card1').classList.add('animated', 'fadeInLeft');
-        document.querySelector('.card2').classList.add('animated', 'fadeInDown');
+        document.querySelector('.card2').classList.add('animated', 'zoomIn');
         document.querySelector('.card3').classList.add('animated', 'fadeInRight');
     }
     if (scroll > 1200) {
         document.querySelector('.card4').classList.add('animated', 'fadeInLeft');
-        document.querySelector('.card5').classList.add('animated', 'fadeInUp');
+        document.querySelector('.card5').classList.add('animated', 'zoomIn');
         document.querySelector('.card6').classList.add('animated', 'fadeInRight');
     }
 });
